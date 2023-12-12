75e8ea5ea2bf5b01e38b413982e4f80609c1e175
AliElgohary <alielgohary35@gmail.com>
2023-12-12 21:21:48 +0200

    adding lab10

--- JS/Day10_lab/server.js
+++ JS/Day10_lab/server.js
@@ -1,18 +1,41 @@
 function Sequence(start, end, steps) {
-    this.start = start;
-    this.end = end;
-    this.steps = steps; 
-    this.generateSeq = function () {
-      let result = [];
-      let currentStep = start;
-      for (let i = start; i <= end; i += steps) {
-        result.push(currentStep);
-        currentStep += steps;
+  this.start = start;
+  this.end = end;
+  this.steps = steps;
+  var result = [];
+
+  this.generateSeq = function () {
+    this.result = [];
+    for (let i = this.start; i <= this.end; i += this.steps) {
+      if (!result.includes(i)) {
+        result.push(i);
       }
-      return result;
-    };
-  }
-  
-  const seq = new Sequence(1, 10, 3);
-  console.log(seq.generateSeq());
-  
+    }
+    return result;
+  };
+
+  this.addValue = function (value) {
+    if (!result.includes(value)) {
+      result.push(value);
+    }
+    return result;
+  };
+
+  this.removeValue = function (value) {
+    const index = result.indexOf(value);
+    if (index > -1) {
+      result.splice(index, 1);
+    }
+    return result;
+  };
+}
+
+const seq = new Sequence(1, 10, 3);
+console.log(seq.generateSeq());
+
+seq.addValue(3);
+seq.addValue(5);
+console.log(seq.generateSeq());
+
+seq.removeValue(3);
+console.log(seq.generateSeq());
